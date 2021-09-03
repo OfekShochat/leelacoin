87af54b3a813c93299ed7a777aa2e49d7de9dd2b
Ofek Shochat <o@shoch.at>
2021-09-03 08:54:49 +0300

    adding struct support for deserialize

--- src/p2p.rs
+++ src/p2p.rs
@@ -1,12 +1,9 @@
-use std::{
-  io::Read,
-  net::{TcpListener, TcpStream},
-};
+use std::{io::{Error, Read, Write}, net::{TcpListener, TcpStream}};
 // use rand::rngs::OsRng;
 use ed25519_dalek::{Keypair, Signature, Signer};
 use log::{error, info};
 use miniz_oxide::{deflate::compress_to_vec, inflate::decompress_to_vec};
-use serde_json::Value;
+use serde::{Deserialize, Serialize};
 
 pub struct Listener {
   keypair: Keypair,
@@ -14,10 +11,27 @@ pub struct Listener {
 
 const BUFFER_SIZE: usize = 65536;
 
-fn get_message(mut stream: TcpStream, buf: &mut [u8; BUFFER_SIZE]) -> Vec<u8> {
+fn get_message(stream: &mut TcpStream, buf: &mut [u8; BUFFER_SIZE]) -> Vec<u8> {
   stream.read(&mut buf[..]).unwrap();
   let stripped = buf.strip_suffix(b"\0").unwrap(); // removing trailing zeros
   decompress_to_vec(stripped).unwrap()
+}
+
+fn send_message(stream: &mut TcpStream, msg: &[u8]) -> Result<(), Error> {
+  let compressed = compress_to_vec(msg, 9);
+  let res = stream.write(&compressed);
+  if res.is_err() {
+    Err(res.unwrap_err())
+  } else {
+    Ok(())
+  }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Message {
+  destiny: String,
+  pubkey: Vec<u8>,
+  signed: Vec<u8>,
 }
 
 impl Listener {
@@ -34,11 +48,11 @@ impl Listener {
     );
     for stream in listener.incoming() {
       match stream {
-        Ok(stream) => {
+        Ok(mut stream) => {
           let mut buf = [0; BUFFER_SIZE];
-          let stripped = get_message(stream, &mut buf);
-          let cmd: Value = serde_json::from_slice(&stripped).unwrap();
-          println!("{}", cmd);
+          let stripped = get_message(&mut stream, &mut buf);
+          let msg: Message = serde_json::from_slice(&stripped).unwrap();
+          send_message(&mut stream, b"poop").unwrap();
         }
         Err(e) => error!("connection failed with {}", e),
       }
